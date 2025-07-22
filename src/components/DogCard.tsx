4fb00ebf78c88f15a89f06874169ce88717481c6
Mark Christian Mallari <mallariftw01@gmail.com>
2025-07-22 15:55:01 +0800

    feat: Optimize image

--- src/components/DogCard.tsx
+++ src/components/DogCard.tsx
@@ -8,7 +8,7 @@ const DogCard = ({ breed, image }: DogProps) => {
     return (
         <div className="ring-1 ring-inset ring-gray-300 rounded-xl bg-white">
             <div className="w-full overflow-hidden rounded-xl">
-                <img src={image} alt={image} className="w-full h-96 object-cover   rounded-t-lg hover:scale-110 transition-all ease-in-out" />
+                <img src={image} alt={breed} loading="lazy" className="w-full h-96 object-cover   rounded-t-lg hover:scale-110 transition-all ease-in-out" />
             </div>
             <div className="flex justify-between p-4 ">
                 <p className="font-medium">{breed}</p>
